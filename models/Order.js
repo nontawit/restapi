0613cd57a1763df7df9d15e2c8583f4b9effda18
nontawit <ntwitmn@gmail.com>
2024-05-18 15:02:48 +0700

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -25,7 +25,7 @@ const orderSchema = new mongoose.Schema({
     orderStatus: {
         type: String,
         required: true,
-        default: 'รอดำเนินการ'
+        default: 'Pending'
     },
     orderDate: {
         type: String,
