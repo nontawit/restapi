34b636a7035a0445510c80673f79d9383d0ae22d
nontawit <ntwitmn@gmail.com>
2024-06-15 01:43:25 +0700

    update new api

--- models/Order.js
+++ models/Order.js
@@ -1,46 +1,12 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    cusName: {
-        type: String,
-        required: true
-    },
-    cusAddress: {
-        type: String,
-        required: true
-    },
-    cusPhone: {
-        type: String,
-        required: true
-    },
-    orderUnit: {
-        type: Number,
-        required: true
-    },
-    dateDelivery: {
-        type: String,
-        required: true
-    },
-    orderStatus: {
-        type: String,
-        required: true,
-        default: 'Pending'
-    },
-    orderDate: {
-        type: String,
-        default: () => formatDateThai(new Date()),
-        required: true
-    },
-}, { versionKey: false });
+    name: String,
+    address: String,
+    phone: String,
+    amount: Number,
+    date: String,
+    status: String
+}, { versionKey: false }); // Disable __v
 
-orderSchema.pre('save', function(next) {
-    const thaiDateParts = this.dateDelivery.split('/');
-    if (thaiDateParts.length === 3) {
-        const formattedDate = `${thaiDateParts[0]}/${thaiDateParts[1]}/${thaiDateParts[2]}`;
-        this.dateDelivery = formattedDate;
-    }
-    next();
-});
-
-const Order = mongoose.model('Order', orderSchema);
-module.exports = Order;
+module.exports = mongoose.model('Order', orderSchema);
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,100 +1,24 @@
 const express = require('express');
+const {
+    createOrder,
+    deleteOrder,
+    updateOrder,
+    getAllOrders,
+    getOrdersByStatus,
+    getTotalOrders,
+    getPendingOrders,
+    getCompletedOrders
+} = require('../controllers/orderController');
+
 const router = express.Router();
-const Order = require('../models/Order');
-const { check, validationResult } = require('express-validator');
 
-//Get all orders
-router.get('/', async (req, res) => {
-    try {
-        const orders = await Order.find();
-        res.json(orders);
-        console.log('Get orders successfully!!');
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
-
-//Get order by ID
-router.get('/:id', getOrder, (req, res) => {
-    res.json(res.order);
-    console.log('Get order by ID successfully!!');
-});
-
-//POST create new order
-router.post('/', async (req, res) => {
-    const order = new Order({
-        cusName: req.body.cusName,
-        cusAddress: req.body.cusAddress,
-        cusPhone: req.body.cusPhone,
-        orderUnit: req.body.orderUnit,
-        dateDelivery: req.body.dateDelivery,
-        // orderStatus: req.body.orderStatus,
-    });
-
-    try {
-        const newOrder = await order.save();
-        res.status(201).json(newOrder);
-        console.log('Create new order successfully!!');
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-//PUT update order
-router.put('/:id', getOrder, async (req, res) => {
-    if (req.body.cusName != null) {
-        res.order.cusName = req.body.cusName;
-    }
-    if (req.body.cusAddress != null) {
-        res.order.cusAddress = req.body.cusAddress;
-    }
-    if (req.body.cusPhone != null) {
-        res.order.cusPhone = req.body.cusPhone;
-    }
-    if (req.body.orderUnit != null) {
-        res.order.orderUnit = req.body.orderUnit;
-    }
-    if (req.body.dateDelivery != null) {
-        res.order.dateDelivery = req.body.dateDelivery;
-    }
-    if (req.body.orderStatus != null) {
-        res.order.orderStatus = req.body.orderStatus;
-    }
-
-    try {
-        const updateOrder = await res.order.save();
-        res.json(updateOrder);
-        console.log('Update order successfully!!');
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-//Delete order
-router.delete('/:id', getOrder, async (req, res) => {
-    try {
-        await res.order.deleteOne();
-        res.json({ message: 'Order delete successfully!!'});
-        console.log('Delete order successfully!!');
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-        console.log('Error, Cannot delete order');
-    }
-});
-
-//Middleware function to get order by ID
-async function getOrder(req, res, next) {
-    let order;
-    try {
-        order = await Order.findById(req.params.id);
-        if (order == null) {
-            return res.status(404).json({ message: 'Order not found' });
-        }
-    } catch (err) {
-        return res.status(500).json({ message: err.message });
-    }
-    res.order = order;
-    next();
-}
+router.post('/', createOrder);
+router.delete('/:id', deleteOrder);
+router.put('/:id', updateOrder);
+router.get('/', getAllOrders);
+router.get('/status/:status', getOrdersByStatus);
+router.get('/dashboard/total-orders', getTotalOrders);
+router.get('/dashboard/pending-orders', getPendingOrders);
+router.get('/dashboard/completed-orders', getCompletedOrders);
 
 module.exports = router;
